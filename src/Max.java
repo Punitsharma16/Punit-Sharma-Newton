deef8f5cf156adaca7c3e0b6e2e05b3d7ac373bf
punit sharma <punitsharma9034908543@gmail.com>
2023-01-30 14:19:47 +0530

    new

---
+++ src/Max.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class Max {
+    static int GetMax(int[] arr){
+        int max = 0;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print(GetMax(arr));
+    }
+}
