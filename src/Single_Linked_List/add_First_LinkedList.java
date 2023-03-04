fd2bdc7fbe3fc120e4f0cc7f5c29a184b2694350
punit sharma <punitsharma9034908543@gmail.com>
2023-03-04 12:13:47 +0530

    new

---
+++ src/Single_Linked_List/add_First_LinkedList.java
@@ -0,0 +1,17 @@
+package Single_Linked_List;
+
+import java.util.*;
+
+public class add_First_LinkedList {
+    public static Node addValueBeforeHead(Node head , int k){
+        if(head == null){
+            System.out.print("Empty List");
+        }
+        else{
+            //create a new Node and store the value;
+            Node new_Node = new Node(k);
+            new_Node.next = head;
+            head = new_Node;
+        }
+        return head;
+    }
